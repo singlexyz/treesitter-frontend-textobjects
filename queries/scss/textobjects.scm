425a2100b50f3a92b1d0aaafcedd79060713612a
nayuxuohz <nayuxuohz@nayuxuohzdeMacBook-Pro.local>
2021-11-26 19:08:25 +0800

    update

--- queries/scss/textobjects.scm
+++ queries/scss/textobjects.scm
@@ -1,16 +1,19 @@
-(rule_set (block) @rule.inner) @rule.outer
-(declaration) @declaration.outer
-
 (rule_set
-  (selectors) @lhs.inner
-  (block) @rhs.inner
-)
+  (block
+    "{" . (_) @_start
+    (_)?
+  @_end "}")
+  (#make-range! "rule.inner" @_start @_end)
+) @rule.outer
+
+(declaration) @declaration.inner
+(declaration) @declaration.outer
 
 (declaration
   (property_name) @lhs.inner
-)
+) @lhs.outer
 
 ((declaration
   (property_name) . (_) @_start
   (_)? @_end .)
-  (#make-range! "rhs.inner" @_start @_end))
+  (#make-range! "rhs.inner" @_start @_end)) @rhs.outer
--- queries/vue/textobjects.scm
+++ queries/vue/textobjects.scm
@@ -1,1 +1,5 @@
-[(self_closing_tag) (start_tag)] (attribute (quoted_attribute_value (attribute_value) @attribute.inner)) @attribute.outer
+[(attribute) (directive_attribute)] @attribute.outer
+  (quoted_attribute_value
+    (attribute_value) @attribute.inner)
+
+(element) @tag.outer
